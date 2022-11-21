95a6d44b178e9977d5f33e308e70350c0ea0b6db
TO053037 <67772806+TO053037@users.noreply.github.com>
2022-11-21 12:35:49 +0900

    add: env configure

--- tuspro9/settings.py
+++ tuspro9/settings.py
@@ -11,16 +11,20 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
+
+env = environ.Env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
-
+env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&ur$lo&=ey$ewpo=e4bq)1ckfyx_z=sfxk@p)7w70_(bh^3pn0'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
