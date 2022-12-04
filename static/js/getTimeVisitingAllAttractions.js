4740ecd00d9ea4718f0ee82d67b325f5ca4a835b
TO053037 <67772806+TO053037@users.noreply.github.com>
2022-12-04 12:46:20 +0900

    add: frontend api

--- static/js/getTimeVisitingAllAttractions.js
+++ static/js/getTimeVisitingAllAttractions.js
@@ -1,28 +1,40 @@
-async function getTimeVisitingAllAttractions(index, model_id) {
+async function getTimeVisitingAllAttractions(model_id) {
 	const queryParam = new URLSearchParams({
-		'index': index,
-		'model_id': model_id,
+		model_id: model_id,
 	});
 	const resJson = await fetch(
 		getTimeVisitingAllAttractionsUrl + "?" + queryParam
 	);
 	const res = await resJson.json();
 
-	if (res.status === 200) {
-		document.getElementById("date-" + index.toString()).innerHTML = res.date;
-		document.getElementById("route-date-" + index.toString()).innerHTML =
-			res.date;
-		if (res.is_visit_all_attractions) {
-			document.getElementById("is-visit-all-" + index.toString() + "-" + model_id.toString()).innerHTML =
-				"可";
+	for (let index = 0; index < 7; index++) {
+		document.getElementById(
+			"date-" + index.toString() + "-" + model_id.toString()
+		).innerHTML = res[index]["date"];
+
+		document.getElementById(
+			"route-date-" + index.toString() + "-" + model_id.toString()
+		).innerHTML = res[index]["date"];
+
+		if (res[index]["is_visit_all_attractions"]) {
+			document.getElementById(
+				"is-visit-all-" + index.toString() + "-" + model_id.toString()
+			).innerHTML = "◯";
 		} else {
-			document.getElementById("is-visit-all-" + index.toString() + "-" + model_id.toString()).innerHTML =
-				"不可";
+			document.getElementById(
+				"is-visit-all-" + index.toString() + "-" + model_id.toString()
+			).innerHTML = "✗";
 		}
-		document.getElementById("time-visit-all-" + index.toString() + "-" + model_id.toString()).innerHTML =
-			res.time;
-		const attractionNameArray = exchangeAttractionNameToNum(res.route);
-		const table = document.getElementById("route-" + index);
+		document.getElementById(
+			"time-visit-all-" + index.toString() + "-" + model_id.toString()
+		).innerHTML = res[index]["time"];
+
+		const attractionNameArray = exchangeAttractionNameToNum(
+			res[index]["route"]
+		);
+		const table = document.getElementById(
+			"route-" + index.toString() + "-" + model_id.toString()
+		);
 		attractionNameArray.forEach((attractionName, index) => {
 			const newTr = document.createElement("tr");
 
@@ -41,11 +53,46 @@ async function getTimeVisitingAllAttractions(index, model_id) {
 
 			table.appendChild(newTr);
 		});
-	} else {
-		document.getElementById("date-" + index.toString()).innerHTML = "no data";
-		document.getElementById("is-visit-all-" + index.toString()).innerHTML =
-			"no data";
-		document.getElementById("time-visit-all-" + index.toString()).innerHTML =
-			"no data";
 	}
+
+	// if (res.status === 200) {
+	// 	document.getElementById("date-" + index.toString()).innerHTML = res.date;
+	// 	document.getElementById("route-date-" + index.toString()).innerHTML =
+	// 		res.date;
+	// 	if (res.is_visit_all_attractions) {
+	// 		document.getElementById("is-visit-all-" + index.toString() + "-" + model_id.toString()).innerHTML =
+	// 			"可";
+	// 	} else {
+	// 		document.getElementById("is-visit-all-" + index.toString() + "-" + model_id.toString()).innerHTML =
+	// 			"不可";
+	// 	}
+	// 	document.getElementById("time-visit-all-" + index.toString() + "-" + model_id.toString()).innerHTML =
+	// 		res.time;
+	// 	const attractionNameArray = exchangeAttractionNameToNum(res.route);
+	// 	const table = document.getElementById("route-" + index);
+	// 	attractionNameArray.forEach((attractionName, index) => {
+	// 		const newTr = document.createElement("tr");
+
+	// 		const newTdIndex = document.createElement("td");
+	// 		const IndexP = document.createElement("p");
+	// 		IndexP.textContent = index + 1;
+	// 		newTdIndex.appendChild(IndexP);
+
+	// 		const newTdName = document.createElement("td");
+	// 		const NameP = document.createElement("p");
+	// 		NameP.textContent = attractionName;
+	// 		newTdName.appendChild(NameP);
+
+	// 		newTr.appendChild(newTdIndex);
+	// 		newTr.appendChild(newTdName);
+
+	// 		table.appendChild(newTr);
+	// 	});
+	// } else {
+	// 	document.getElementById("date-" + index.toString()).innerHTML = "no data";
+	// 	document.getElementById("is-visit-all-" + index.toString()).innerHTML =
+	// 		"no data";
+	// 	document.getElementById("time-visit-all-" + index.toString()).innerHTML =
+	// 		"no data";
+	// }
 }
--- static/js/onClickDate.js
+++ static/js/onClickDate.js
@@ -1,22 +1,26 @@
-function onclickDate(index) {
+function onclickDate(index, model_id) {
 	className = "hidden";
 	for (let i = 0; i < 7; i++) {
-		let table = document.getElementById("route-" + i);
-		let p = document.getElementById("route-date-" + i);
-		if (i == index) {
-			if (table.classList.contains("hidden")) {
-				table.classList.remove("hidden");
+		for (let j = 0; j < 3; j ++) {
+			let table = document.getElementById("route-" + i + "-" + j);
+			let p = document.getElementById("route-date-" + i + "-" + j);
+			console.log(table);
+			if (i == index && j == model_id) {
+				if (table.classList.contains("hidden")) {
+					table.classList.remove("hidden");
+				}
+				if (p.classList.contains("hidden")) {
+					p.classList.remove("hidden");
+				}
+			} else {
+				if (!table.classList.contains("hidden")) {
+					table.classList.add("hidden");
+				}
+				if (!p.classList.contains("hidden")) {
+					p.classList.add("hidden");
+				}
 			}
-			if (p.classList.contains("hidden")) {
-				p.classList.remove("hidden");
-			}
-		} else {
-			if (!table.classList.contains("hidden")) {
-				table.classList.add("hidden");
-			}
-			if (!p.classList.contains("hidden")) {
-				p.classList.add("hidden");
-			}
+				
 		}
 	}
 }
