164ce240fdaad296bd37a368a5b74ff2908d5d4a
TO053037 <67772806+TO053037@users.noreply.github.com>
2023-01-05 10:13:04 +0900

    add: data

--- dairy_routine.py
+++ dairy_routine.py
@@ -5,9 +5,9 @@ import time
 
 
 if __name__ == '__main__':
-    date = datetime.date.today() + datetime.timedelta(days=6)
-    create_expected_wait_time_csv_file(
-        year=date.year, month=date.month, day=date.day)
     date = datetime.date.today() - datetime.timedelta(days=1)
     create_wait_time_all_attractions_csv_file(
+        year=date.year, month=date.month, day=date.day)
+    date = datetime.date.today() + datetime.timedelta(days=6)
+    create_expected_wait_time_csv_file(
         year=date.year, month=date.month, day=date.day)
