20e2a4618087886a7ff8d0c27ccdb51ba40c3c9a
TO053037 <67772806+TO053037@users.noreply.github.com>
2023-01-11 09:13:58 +0900

    add: data

--- dairy_routine.py
+++ dairy_routine.py
@@ -1,7 +1,6 @@
 from util.create_wait_time_all_atractions_csv_file import create_wait_time_all_attractions_csv_file
 from util.create_expected_wait_time_csv_file import create_expected_wait_time_csv_file
 import datetime
-import time
 
 
 if __name__ == '__main__':
